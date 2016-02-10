dcf5b9e24f357fee6ecc4ac4f37ab6744aa55672
jangdan <minhyukjang@gmail.com>
2016-02-10 16:34:29 +0900

    made root nodes more flexible

--- src/Tree.js
+++ src/Tree.js
@@ -2,9 +2,13 @@ var Tree = function(name){
 
 	this.name = name;
 
+}
 
-	this.root = new Node("root");
-	
+
+Tree.prototype.setRoot(root){
+
+	this.root = root;
+
 	this.root.parent = null;
 	this.root.depth = 0;
 
